8201c97aa248a103cd84fa49ea4d503e1d0464ee
Kira <iallegautier@gmail.com>
2019-12-06 19:48:22 +0100

    make it better

--- btpland/settings.py
+++ btpland/settings.py
@@ -133,7 +133,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    "compressor.finders.CompressorFinder",
 )
 
 LOGIN_REDIRECT_URL ='/administrator'
