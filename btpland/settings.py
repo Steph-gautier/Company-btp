41475f41b6e8c70992fac4aab213822ff16a376d
Kira <iallegautier@gmail.com>
2019-12-06 19:44:47 +0100

    make it better

--- btpland/settings.py
+++ btpland/settings.py
@@ -125,11 +125,16 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
+)
 
 LOGIN_REDIRECT_URL ='/administrator'
 LOGOUT_REDIRECT_URL ='/'
